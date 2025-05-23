57d3e757c4eea89a632ea394d7d1f6d116b197f6
strivehardest <ibrahimah2011@hotmail.com>
2025-05-23 08:32:05 +0000

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define two variables
+number1 = 10
+number2 = 5
+
+# Perform basic arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
